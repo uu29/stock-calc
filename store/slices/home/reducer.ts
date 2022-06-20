38abed5ff58ecc2686ffcf062d26964c67b9004b
uu29 <uu5474@gmail.com>
2022-06-20 17:21:19 +0900

    feat: reducer 인터페이스 수정, actions 추가

--- store/slices/home/reducer.ts
+++ store/slices/home/reducer.ts
@@ -1,47 +1,88 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import router from "next/router";
 import { FeatureKey } from "../../featureKey";
 
+export const totalStock = {
+  average: "average",
+  amount: "amount",
+  ror: "ror",
+  total_quantity: "total_quantity",
+} as const;
+
+export type TotalStockType = typeof totalStock[keyof typeof totalStock];
+
+export interface ITotalStock {
+  average: number;
+  amount: number;
+  ror: number;
+  total_quantity: number;
+}
+
+export const currentStock = {
+  market_price: "market_price",
+  purchase_price: "purchase_price",
+  holding_quantity: "holding_quantity",
+} as const;
+
+export interface ICurrentStock {
+  market_price: number;
+  purchase_price: number;
+  holding_quantity: number;
+}
+
+export type CurrentStockType = typeof currentStock[keyof typeof currentStock];
+
+export const tradingStock = {
+  trading_price: "trading_price",
+  trading_quantity: "trading_quantity",
+} as const;
+
+export type TradingStockType = typeof tradingStock[keyof typeof tradingStock];
+
+interface ITradingStock {
+  trading_price: number;
+  trading_quantity: number;
+}
+
 export interface HomeState {
-  total: {
-    average: number; // 최종 평단가
-    amount: number; // 매수 총액
-    ror: number; // 최종 수익률
-    quantity: number; // 최종 수량
-  };
-  bought: {
-    market_price: number; // 현재 시장가
-    purchase_price: number; // 현재 내 평단
-    quantity: number; // 보유 수량
-  };
-  trading: {
-    purchase_price: number; // 추매 가격
-    quantity: number; // 추매 수
-  };
+  total_stock: ITotalStock;
+  current_stock: ICurrentStock;
+  trading_stock: ITradingStock;
 }
 
 export const initialState: HomeState = {
-  total: {
+  total_stock: {
     average: 0,
     amount: 0,
     ror: 0,
-    quantity: 0,
+    total_quantity: 0,
   },
-  bought: {
+  current_stock: {
     market_price: 0,
     purchase_price: 0,
-    quantity: 0,
+    holding_quantity: 0,
   },
-  trading: {
-    purchase_price: 0,
-    quantity: 0,
+  trading_stock: {
+    trading_price: 0,
+    trading_quantity: 0,
   },
 };
 
 export const homeSlice = createSlice({
   name: FeatureKey.home,
   initialState,
-  reducers: {},
+  reducers: {
+    setTotalStock(state, action: PayloadAction<ITotalStock>) {
+      state.total_stock = { ...action.payload };
+    },
+    setCurrentStock(state, action: PayloadAction<ICurrentStock>) {
+      state.current_stock = { ...action.payload };
+    },
+    setTradingStock(state, action: PayloadAction<ITradingStock>) {
+      state.trading_stock = { ...action.payload };
+    },
+  },
 });
+
+export const { setTotalStock, setCurrentStock, setTradingStock } = homeSlice.actions;
 
 export default homeSlice.reducer;
